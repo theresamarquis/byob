45fb084c2c8eeb5d23f383e69fc526e95fbd5e8a
tmcjunkinmarquis <tmcjunkinmarquis@gmail.com>
2018-08-29 17:41:43 -0600

    Add endpoint for get for senators

--- server.js
+++ server.js
@@ -6,6 +6,28 @@ const database = require('knex')(configuration);
 
 app.set('port', process.env.PORT || 3000)
 app.locals.title = 'byob'
+
+app.get('/api/v1/senators', (request, response) => {
+  database('senators').select()
+    .then((senators)=>{
+      response.status(200).json(senators)
+    })
+    .catch((err)=>{
+      response.status(500).json({err})
+    })
+})
+
+
+
+
+
+
+
+
+
+
+
+
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on ${app.get('port')}.`);
 });
