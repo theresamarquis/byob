25b8c1debc17555b6286c8083c61d35f0d348cb5
tmcjunkinmarquis <tmcjunkinmarquis@gmail.com>
2018-08-29 17:54:30 -0600

    Add enpoints for id for states and senators

--- server.js
+++ server.js
@@ -27,6 +27,34 @@ app.get('/api/v1/states', (request, response)=>{
   })
 })
 
+app.get('/api/v1/senators/:id', (request, response)=>{
+  database('senators').where('id', request.params.id).select()
+  .then(senators=>{
+    if(senators.length) {
+      response.status(200).json(senators)
+    }else{
+      response.status(404).json({error: `Could not find senator with id ${request.params.id}`})
+    }
+  })
+  .catch(err=>{
+    response.status(500).json({err})
+  })
+})
+
+app.get('/api/v1/states/:id', (request, response)=>{
+  database('states').where('id', request.params.id).select()
+  .then(states=>{
+    if(states.length) {
+      response.status(200).json(states)
+    }else{
+      response.status(404).json({error: `Could not find state with id ${request.params.id}`})
+    }
+  })
+  .catch(err=>{
+    response.status(500).json({err})
+  })
+})
+
 
 
 
