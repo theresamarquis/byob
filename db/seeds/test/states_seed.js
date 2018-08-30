d02cf5bd4c519fee3d333f24a4da514a3b80d420
tmcjunkinmarquis <tmcjunkinmarquis@gmail.com>
2018-08-30 17:18:59 -0600

    Seeded test db with states

---
+++ db/seeds/test/states_seed.js
@@ -0,0 +1,41 @@
+const stateData = require('../../../states.js')
+
+const createStates = (knex, state) => {
+  return knex('states').insert({
+    state_name: state.name,
+    population: state.population,
+    capital: state.capital
+  }, 'id')
+    .then(stateId => {
+      let senatorsPromises = [];
+
+      state.senators.forEach(senator => {
+        senatorsPromises.push(createSenator(knex, {
+          senator_name: senator.name,
+          party: senator.party,
+          state_id: stateId[0]
+        })
+        )
+      });
+      return Promise.all(senatorsPromises)
+    })
+};
+
+const createSenator = (knex, senator) => {
+  return knex('senators').insert(senator)
+};
+
+exports.seed = function (knex, Promise) {
+  return knex('senators').del()
+    .then(() => knex('states').del())
+    .then(() => {
+      let statePromises = [];
+
+      stateData.forEach(state => {
+        statePromises.push(createStates(knex, state));
+      });
+      return Promise.all(statePromises)
+    })
+    .catch(error => console.log(`Error seeding data: ${error}`));
+
+};
