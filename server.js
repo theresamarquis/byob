aa336365cde2c29c0bd5afcc696627f7fd48731e
tmcjunkinmarquis <tmcjunkinmarquis@gmail.com>
2018-09-01 12:25:01 -0600

    Require jsonwebtoken

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken';)
 const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile')[environment];
 const database = require('knex')(configuration);
