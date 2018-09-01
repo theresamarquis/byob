1dcc45538fca70018bda147bd33a98860ceed081
Marika Ross <marika.ross@gmail.com>
2018-09-01 10:16:50 -0600

    Made Patch for states

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -78,7 +78,102 @@ describe('API Routes', () => {
         done()
       })
     })
+ })
+
+  describe('GET /api/v1/senators/:id', () => {
+  it('should return a single senator', done => {
+    chai.request(server)
+    .get('/api/v1/senators/1')
+    .end((err, response) => {
+      response.should.have.status(200);
+      response.should.be.json;
+      response.body.should.be.a('array');
+      response.body.length.should.equal(1);
+      response.body[0].should.have.property('senator_name');
+      response.body[0].senator_name.should.equal('Doug Jones');
+      response.body[0].should.have.property('party');
+      response.body[0].party.should.equal('D');
+      response.body[0].should.have.property('id');
+      response.body[0].id.should.equal(1);
+      response.body[0].should.have.property('state_id');
+      response.body[0].state_id.should.equal(1);
+      done()
+    })
   })
+  it('should return a 404 for a route that does not exist', done => {
+    chai.request(server)
+    .get('/sad')
+    .end((err, response) => {
+      response.should.have.status(404);
+    done()
+    })
+  })
+})
+
+
+describe('GET /api/v1/states/:id', () => {
+  it('should return a single state', done => {
+    chai.request(server)
+    .get('/api/v1/states/2')
+    .end((err, response) => {
+      response.should.have.status(200);
+      response.should.be.json;
+      response.body.should.be.a('array');
+      response.body.length.should.equal(1);
+      response.body[0].should.have.property('state_name');
+      response.body[0].state_name.should.equal('Alaska');
+      response.body[0].should.have.property('capital');
+      response.body[0].capital.should.equal('Juneau');
+      response.body[0].should.have.property('population');
+      response.body[0].population.should.equal(739759);
+      response.body[0].should.have.property('id');
+      response.body[0].id.should.equal(2);
+      done()
+    })
+  })
+  
+  it('should return a 404 for a route that does not exist', done => {
+        chai.request(server)
+        .get('/sad')
+        .end((err, response) => {
+            response.should.have.status(404);
+          done()
+          })
+      })
+  })
+
+
+describe('PATCH /api/v1/states/:id', () => {
+  it('should update a state', done => {
+    chai.request(server)
+    .patch('/api/v1/states/1')
+    .send({
+      population: 2
+    })
+    .end((err, response) => {
+      response.should.have.status(201);
+      response.should.be.json;
+      response.body.should.be.a('object');
+      done()
+    })
+  })
+
+  it('should not update a state if there is an id field in the request', done => {
+    chai.request(server)
+    .patch('/api/v1/senators/1')
+    .send({
+      id: 2
+    })
+    .end((err, response) => { 
+      response.should.have.status(422);
+      response.should.be.json;
+      response.body.should.be.a('object');
+      response.body.should.have.property('error');
+      response.body.error.should.equal('You cannot update the id field.');  
+      done()
+    })
+  })  
+})
 
 
 });
