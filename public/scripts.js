5673c4627ce7e015144419f1c4e995a34859666c
tmcjunkinmarquis <tmcjunkinmarquis@gmail.com>
2018-09-01 13:46:42 -0600

    Add getJwt function

---
+++ public/scripts.js
@@ -0,0 +1,29 @@
+const submitHandler = document.querySelector('.button')
+
+const getJwt = (event) => {
+  event.preventDefault();
+  const emailInput = document.querySelector('#email');
+  const email = emailInput.value;
+  const isTuring = email.split('@')[1];
+
+  if (!email) {
+    document.querySelector('.feedback').innerText = 'Please fill out required fields.';
+  } else if (isTuring !== 'turing.io') {
+    document.querySelector('.feedback').innerText = 'Must be a Turing staff member.';
+  } else {
+    return fetch('api/v1/authorize', {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({email: email, appName: "byob"})
+    })
+      .then(response => response.json())
+      .then(result => {
+        document.querySelector('.feedback').innerText = 'jwt:' +  JSON.stringify(result.token);
+      })
+      .catch(error => console.log(error));
+  }
+};
+
+submitHandler.addEventListener('click', getJwt);
